546adc8a201ae2f395af10a3026098665d0ee1b2
dujiajiyi <towangyanqing@gmail.com>
2017-05-17 14:36:51 +0800

    添加可以接受文件，和文件夹,优化

--- src/file.rs
+++ src/file.rs
@@ -40,14 +40,14 @@ pub fn download<A: ToSocketAddrs, S: AsRef<Path>>(addr: A, to_path: S, packet_id
             fs::create_dir(&base_file_location)?;
         }
         let root_path = base_file_location;
-
-        download_file(&mut stream, &root_path);
+        let mut buffer = BufReader::new(stream);
+        download_file(&mut buffer, &root_path);
     }
     Ok(())
 }
 
-fn download_file<S>(mut stream: &mut TcpStream, next_base_path: S) -> Result<()> where S: AsRef<Path> {
-    if let Some(header_size_str) = read_delimiter(&mut stream){
+fn download_file<S>(mut stream : & mut BufReader<TcpStream>, next_base_path: S) -> Result<()> where S: AsRef<Path> {
+    if let Some(header_size_str) = read_delimiter(stream){
         let mut next_path: PathBuf = next_base_path.as_ref().to_path_buf();
         let header_size = u64::from_str_radix(&header_size_str, 16).unwrap();
         info!("header_size {:?}", header_size);
@@ -59,27 +59,22 @@ fn download_file<S>(mut stream: &mut TcpStream, next_base_path: S) -> Result<()>
         info!("header context {:?}", v);
         if file_attr == IPMSG_FILE_DIR {
             next_path.push(file_name);
-            //download
             if !next_path.exists() {
                 fs::create_dir(&next_path)?;
             }
-            info!("创建文件夹{:?}", next_path);
+            info!("crate dir{:?}", next_path);
             download_file(&mut stream, next_path)?;
         }else if file_attr == IPMSG_FILE_REGULAR {
             let tmp_path = next_path.clone();
-            info!("下载普通文件基础目录 {:?}", tmp_path);
+            info!("base dir {:?}", tmp_path);
             next_path.push(file_name);
             //create path
             read_bytes_to_file(&mut stream, file_size, &next_path);
-            info!("下载普通文件 {:?}", next_path);
-            info!("下载普通文件基础目录 {:?}", tmp_path);
             //传入下一个的是目录，可能得在去掉文件名，在往下传
             download_file(&mut stream, tmp_path)?;
         }else if file_attr == IPMSG_FILE_RETPARENT  {
-            //download_file(&mut stream, root)
-            //next -- root
             //root 从哪里读取
-            info!("回到父目录");
+            info!("back to parent");
             next_path.pop();
             download_file(&mut stream, next_path)?;
         }
@@ -87,64 +82,28 @@ fn download_file<S>(mut stream: &mut TcpStream, next_base_path: S) -> Result<()>
     Ok(())
 }
 
-///
-/// 1、接收为0的时候，就是接受完成了
-/// 2、报错的时候
-fn read_delimiter(stream: &mut TcpStream) -> Option<String> {
+fn read_delimiter(mut stream : & mut BufReader<TcpStream>) -> Option<String> {
     let mut s_buffer = Vec::new();
-    let mut i = 0;
-    let mut err_flag = false;
-    let mut read_over_success = false;
-    loop {
-        let mut buffer = [0; 1];
-        let num = stream.read(&mut buffer[..]).unwrap();
-        if num == 0&&i == 0 {
-            read_over_success = true;
-            break;
-        }
-        if char::from(buffer[0]) == ':' {
-            break;
-        }else{
-            s_buffer.push(buffer[0]);
-        }
-        if i > 20 {
-            err_flag = true;
-            break;
-        }
-        i = i + 1;
-    }
-    if err_flag {
-        panic!("error");//解析错误
-    }else {
-        if read_over_success {
-            None
+    if let Ok(buffer) = stream.read_until(b':', &mut s_buffer) {
+        if buffer != 0usize {
+            s_buffer.pop();
+            Some(GB18030.decode(s_buffer.as_slice(), DecoderTrap::Ignore).unwrap())
         }else {
-            Some(String::from_utf8(s_buffer).unwrap())
+            None
         }
-    }
-}
-
-/// 1、接收为0的时候，就是接受完成了
-/// 2、报错的时候
-fn read_delimiter1(stream: &mut TcpStream) -> Option<String> {
-    let mut s_buffer = Vec::new();
-    let mut reader = BufReader::new(stream);
-    if let Ok(buffer) = reader.read_until(b':', &mut s_buffer) {
-        Some(String::from_utf8(s_buffer).unwrap())
     }else {
         None
     }
 }
 
-fn read_bytes(stream:&mut TcpStream, len: u64) -> String {
+fn read_bytes(mut stream : & mut BufReader<TcpStream>, len: u64) -> String {
     let mut s_buffer = Vec::new();
     let mut handler = stream.take(len);
     handler.read_to_end(&mut s_buffer);
     GB18030.decode(s_buffer.as_slice(), DecoderTrap::Ignore).unwrap()
 }
 
-fn read_bytes_to_file<S>(stream:&mut TcpStream, len: u64, file_path: S) where S: AsRef<Path> {
-    info!("将要创建的文件 {:?}", &file_path.as_ref());
+fn read_bytes_to_file<S>(mut stream : & mut BufReader<TcpStream>, len: u64, file_path: S) where S: AsRef<Path> {
     let mut f: File = File::create(&file_path).unwrap();
     let mut handler = stream.take(len as u64);
     let mut buf = [0; 1024 * 4];
