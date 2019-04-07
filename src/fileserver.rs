ada0ffc39dec3257e43f063064f43fb1f3165f5b
dujiajiyi <towangyanqing@gmail.com>
2019-04-07 13:48:57 +0800

    修复发送文件夹文件比较多，中断的问题

--- src/fileserver.rs
+++ src/fileserver.rs
@@ -111,16 +111,16 @@ impl FileServer {
                 search_result = result.cloned();
             }
             if let Some(result_share_file) = search_result {
-                let file_info = result_share_file.file_info.iter().find(|ref f| f.file_id == file_id as u32);
+                let file_info = result_share_file.file_info.iter().find(|f| f.file_id == file_id as u32);
                 if let Some(file_info) = file_info {
                     let mut f: File = File::open(&file_info.file_name).unwrap();
-                    let mut buf = [0; 1024 * 10];
+                    let mut buf = [0; 1024];
                     let mut buffer = BufWriter::new(stream_echo);
                     while let Ok(bytes_read) = f.read(&mut buf) {
                         if bytes_read == 0 { break; }
                         buffer.write(&buf[..bytes_read]).unwrap();
+                        buffer.flush().unwrap();
                     }
-                    buffer.flush().unwrap();
                 }
             }
         }
@@ -138,11 +138,12 @@ pub fn send_dir(root_path: &PathBuf, mut buffer : & mut BufWriter<TcpStream>) {
                 let header = make_header(sub);
                 buffer.write(util::utf8_to_gb18030(&header).as_slice()).unwrap();
                 info!("{:?}", header);
-                let mut buf = [0; 1024 * 10];
+                let mut buf = [0; 1024];
                 let mut f: File = File::open(sub).unwrap();
                 while let Ok(bytes_read) = f.read(&mut buf) {
                     if bytes_read == 0 { break; }
                     buffer.write(&buf[..bytes_read]).unwrap();
+                    buffer.flush().unwrap();
                 }
             }else {
                 send_dir(sub, &mut buffer);
